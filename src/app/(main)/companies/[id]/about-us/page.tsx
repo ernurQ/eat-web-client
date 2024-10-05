410acace16051ffc3163974defd995be3035207c
ernurQ <garifullin.ernur@gmail.com>
2024-10-05 21:13:02 +0500

    feat(companies page): add request to get company info

--- src/app/(main)/companies/[id]/about-us/page.tsx
+++ src/app/(main)/companies/[id]/about-us/page.tsx
@@ -1,8 +1,20 @@
-export default function CompanyAboutUsPage() {
+'use client'
+
+import { useGetCompanyInfoQuery } from '@/entities/company'
+
+interface Props {
+	params: { id: string }
+}
+
+export default function CompanyAboutUsPage({ params: { id } }: Props) {
+	const { data: company } = useGetCompanyInfoQuery({ id })
+
+	if (!company) return null
+
 	return (
 		<section>
 			<div>company profile image</div>
-			<h2>описание</h2>
+			<h2>{company.description}</h2>
 			<h3>Связаться с нами</h3>
 		</section>
 	)
--- src/app/(main)/companies/[id]/layout.tsx
+++ src/app/(main)/companies/[id]/layout.tsx
@@ -1,17 +1,25 @@
+'use client'
+
 import Link from 'next/link'
 import { PropsWithChildren } from 'react'
 
 import { routes } from '@/shared/config'
 import { Header } from '@/shared/ui'
 
+import { useGetCompanyInfoQuery } from '@/entities/company'
+
 interface Props extends PropsWithChildren {
 	params: { id: string }
 }
 
 export default function CompanyLayout({ children, params: { id } }: Props) {
+	const { data: company } = useGetCompanyInfoQuery({ id })
+
+	if (!company) return null
+
 	return (
 		<>
-			<Header level={1}>Company Name</Header>
+			<Header level={1}>{company.name}</Header>
 			<div className='flex justify-around'>
 				<Link href={routes.companyProducts(id)}>Товары</Link>
 				<Link href={routes.company(id)}>О заведении</Link>
