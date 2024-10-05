727d36a6c5cf833f5b782b3844408bc96bc9c445
ernurQ <garifullin.ernur@gmail.com>
2024-10-05 21:12:48 +0500

    feat(profile page): add request to get user info

--- src/app/(main)/profile/layout.tsx
+++ src/app/(main)/profile/layout.tsx
@@ -1,15 +1,25 @@
+'use client'
+
 import Link from 'next/link'
 import { PropsWithChildren } from 'react'
 
 import { routes } from '@/shared/config'
 
+import { useGetUserInfoQuery } from '@/entities/user'
+
 export default function ProfileLayout({ children }: PropsWithChildren) {
+	const { data: user } = useGetUserInfoQuery()
+
+	if (!user) return null
+
 	return (
 		<>
 			<section>
 				<div className={'bg-amber-200 flex justify-around p-7'}>
 					<div>user profile image</div>
-					<div>user name surname</div>
+					<div>
+						{user.name} {user.surname}
+					</div>
 				</div>
 
 				<div className={'200 flex justify-around p-2'}>
--- src/app/(main)/profile/me/page.tsx
+++ src/app/(main)/profile/me/page.tsx
@@ -1,3 +1,17 @@
+'use client'
+
+import { useGetUserInfoQuery } from '@/entities/user'
+
 export default function ProfilePage() {
-	return <>Личные данные</>
+	const { data: user } = useGetUserInfoQuery()
+
+	if (!user) return null
+
+	return (
+		<section>
+			<div>name: {user.name}</div>
+			<div>surname: {user.surname}</div>
+			<div>phone: {user.phone}</div>
+		</section>
+	)
 }
