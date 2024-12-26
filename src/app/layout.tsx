4b1d3c246642ba0e03b7dd895cbd0f326adc6e47
enrur1 <garifullin.ernur@gmail.com>
2024-12-26 10:55:59 +0500

    init project

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next'
+import { Open_Sans } from 'next/font/google'
+import { PropsWithChildren } from 'react'
+
+import './globals.css'
+
+const font = Open_Sans({
+	subsets: ['cyrillic'],
+	weight: ['400', '700']
+})
+
+export const metadata: Metadata = {
+	title: 'Eat web',
+	description: 'Connecting Sellers and Buyers to Prevent Food Waste'
+}
+
+export default function RootLayout({ children }: Readonly<PropsWithChildren>) {
+	return (
+		<html lang='ru'>
+			<body className={font.className}>{children}</body>
+		</html>
+	)
+}
